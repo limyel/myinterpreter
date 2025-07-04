bbe5f909fbb30ba0657d0b1c6ac5ee554ba41dae
limyel <limyel@outlook.com>
2025-07-04 17:46:38 +0800

    feat(lexer): 实现词法分析器
    - 新增 lexer包，实现词法分析器功能
    - 添加 Token 结构体和相关类型
    - 实现关键词查找和标识符解析
    - 支持数字和特殊字符的词法分析
    - 添加词法分析器测试用例

---
+++ lexer/lexer.go
@@ -0,0 +1,110 @@
+package lexer
+
+import "github.com/limyel/myinterpreter/token"
+
+// Lexer 词法分析器
+// todo 支持 Unicode 字符
+type Lexer struct {
+	input        string
+	position     int  // 所输入字符串中的当前位置（指向当前字符）
+	readPosition int  // 所输入字符串中的当前读取位置（指向当前字符之后的一个字符，词法分析器需要）
+	ch           byte // 当前正在查看的字符
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
+	l.readChar() // 读取第一个字符，初始化
+	return l
+}
+
+// readChar 读取下一个字符
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// readIdentifier 读取标识符
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// NextToken 将当前字符解析为词法单元，读取下一个字符
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = token.New(token.ASSIGN, l.ch)
+	case ';':
+		tok = token.New(token.SEMICOLON, l.ch)
+	case '(':
+		tok = token.New(token.LPAREN, l.ch)
+	case ')':
+		tok = token.New(token.RPAREN, l.ch)
+	case ',':
+		tok = token.New(token.COMMA, l.ch)
+	case '+':
+		tok = token.New(token.PLUS, l.ch)
+	case '{':
+		tok = token.New(token.LBRACE, l.ch)
+	case '}':
+		tok = token.New(token.RBRACE, l.ch)
+	case 0:
+		tok.Type = token.EOF
+		tok.Literal = ""
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// LookupIdent 中已经执行了 readChar()
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = token.New(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
+
+// isLetter 是否是字符
+func isLetter(ch byte) bool {
+	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+}
+
+// isDigit 是否是数字
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+// skipWhitespace 跳过空白字符
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
--- token/token.go
+++ token/token.go
@@ -35,3 +35,23 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+func New(tokenType TokenType, ch byte) Token {
+	return Token{
+		tokenType,
+		string(ch),
+	}
+}
